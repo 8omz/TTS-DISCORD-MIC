7eed6c711583f308ea1b061272acef00b88429e6
Omar S <sakho.omard@gmail.com>
2025-07-06 02:37:16 -0400

    Created MVP

---
+++ main.py
@@ -0,0 +1,5 @@
+import keyboard as kb
+from ui import open
+kb.add_hotkey("ctrl+space",open)
+
+kb.wait()
--- ui.py
+++ ui.py
@@ -2,16 +2,45 @@
 import customtkinter as ctk
 
 class TTSpopup(ctk.CTk):
+    
     def __init__(self):
         super().__init__()
+        # constants for screen width and height
         SWIDTH = self.winfo_screenwidth()
         SHEIGHT = self.winfo_screenheight()
-        WINWIDTH = 400
+        # and window width and height
+        WINWIDTH = 700
         WINHEIGHT = 80
+        x = (SWIDTH - WINWIDTH) //2
+        y = (SHEIGHT - WINHEIGHT) //4
         
         self.overrideredirect(True)
         
-        x = (SWIDTH - WINWIDTH) //2
-        y = (SHEIGHT - WINHEIGHT) //2
-        
+        # Set the geometry of the window
+        # to center it on the screen
         self.geometry(f"{WINWIDTH}x{WINHEIGHT}+{x}+{y}")
+        self.config(bg="black")
+        self.attributes("-transparentcolor", "black")
+        
+        # Create a custom entry widget
+        self.entry = ctk.CTkEntry(self, placeholder_text="Input your message...",width=700,height=80)
+        self.entry.pack(padx= 10, pady = 10)
+        self.after(100, self._force_focus)
+        
+        # returns string written in the Entry
+       
+        self.entry.bind("<Return>",self.on_enter)
+
+    #no clue what this does 
+    def _force_focus(self):
+        self.lift()               
+        self.entry.focus_force()  
+     
+    def on_enter(self,event):
+        string = self.entry.get()
+        print(string)
+        self.destroy()
+        return string
+def open() :  
+    app = TTSpopup()       
+    app.mainloop()
